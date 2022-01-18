59a4f58344972dd3cf146b54a857d5f8c626997e
Gudmundur <gb9@pm.me>
2022-01-18 02:16:11 +0100

    Upload S7 to github

--- s7_scalable_applications/exercise_files/lfw_dataset.py
+++ s7_scalable_applications/exercise_files/lfw_dataset.py
@@ -5,33 +5,59 @@ import argparse
 import time
 
 import numpy as np
-import torch
 from PIL import Image
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
 
+import glob
+import os
+
+# MATPLOTLIB
+import matplotlib.pyplot as plt
+
+# TORCH
+import torch
+from torchvision.utils import make_grid
+import torchvision.transforms.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torchvision import transforms
 
 class LFWDataset(Dataset):
     def __init__(self, path_to_folder: str, transform) -> None:
-        # TODO: fill out with what you need
+
         self.transform = transform
+        # gather all the images
+        folder_path = os.path.join(path_to_folder, "**/*.jpg")
+        self.paths = glob.glob(folder_path)
         
     def __len__(self):
-        return None # TODO: fill out
+        return len(self.paths)
     
     def __getitem__(self, index: int) -> torch.Tensor:
-        # TODO: fill out
-        return self.transform(img)
+        # get paths by index
+        path = self.paths[index]
+        image = Image.open(path)
+        return self.transform(image)
+
+
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-path_to_folder', default='', type=str)
+    parser.add_argument('-path_to_folder', default='./lfw', type=str)
     parser.add_argument('-batch_size', default=512, type=int)
-    parser.add_argument('-num_workers', default=None, type=int)
-    parser.add_argument('-visualize_batch', action='store_true')
-    parser.add_argument('-get_timing', action='store_true')
-    parser.add_argument('-batches_to_check', default=100, type=int)
+    parser.add_argument('-num_workers', default=1, type=int)
+    parser.add_argument('-visualize_batch', default=False, action='store_true')
+    parser.add_argument('-get_timing', default=True, action='store_true')
+    parser.add_argument('-batches_to_check', default=2, type=int)
     
     args = parser.parse_args()
     
@@ -44,16 +70,28 @@ if __name__ == '__main__':
     dataset = LFWDataset(args.path_to_folder, lfw_trans)
     
     # Define dataloader
-    dataloader = DataLoader(
-        dataset, 
-        batch_size=args.batch_size, 
-        shuffle=False,
-        num_workers=args.num_workers
-    )
-    
     if args.visualize_batch:
-        # TODO: visualize a batch of images
-        pass
+        data_loader = DataLoader(
+            dataset, 
+            batch_size=args.batch_size, 
+            shuffle=True,
+            num_workers=args.num_workers
+        )
+
+        for image in data_loader:
+
+            grid = make_grid(image)
+            if not isinstance(grid, list):
+                grid = [grid]
+            fig, axes = plt.subplots(ncols=len(grid), squeeze=False)
+            for (i, image) in enumerate(grid):
+                image = image.detach()
+                image = F.to_pil_image(image)
+                axes[0, i].imshow(np.asarray(image))
+            plt.show()
+
+            break
+
         
     if args.get_timing:
         # lets do some repetitions
